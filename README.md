1da585ca9e35d329638c11610b3f3e1e6ff95cfe
caokun <ck18336094553@163.com>
2024-11-27 15:16:13 +0800

    初始化

--- README.md
+++
@@ -1,2 +0,0 @@
-# hs-saas-ckuidocs-web
-CaoKunTSVue3UI是一款结合天盛公司项目技术架构，可快速开发企业级后台管理系统的UI组件库
---
+++ assets/guild_quickstart.md.5996c7af.lean.js
@@ -0,0 +1,20 @@
+import{_ as a,c as n,e as l,o as e}from"./app.a014696f.js";const d=JSON.parse('{"title":"\u5FEB\u901F\u5F00\u59CB","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u7528\u6CD5","slug":"\u7528\u6CD5"},{"level":2,"title":"\u5BFC\u5165\u540E\u518D\u9875\u9762\u4E2D\u4F7F\u7528","slug":"\u5BFC\u5165\u540E\u518D\u9875\u9762\u4E2D\u4F7F\u7528"}],"relativePath":"guild/quickstart.md"}'),t={name:"guild/quickstart.md"};function p(o,s,c,i,r,C){return e(),n("div",null,s[0]||(s[0]=[l(`<h1 id="\u5FEB\u901F\u5F00\u59CB" tabindex="-1">\u5FEB\u901F\u5F00\u59CB <a class="header-anchor" href="#\u5FEB\u901F\u5F00\u59CB" aria-hidden="true">#</a></h1><p>\u672C\u8282\u5C06\u4ECB\u7ECD\u5982\u4F55\u5728\u9879\u76EE\u4E2D\u4F7F\u7528 CaoKunTSVue3UI</p><h2 id="\u7528\u6CD5" tabindex="-1">\u7528\u6CD5 <a class="header-anchor" href="#\u7528\u6CD5" aria-hidden="true">#</a></h2><div class="language-"><span class="copy"></span><pre><code><span class="line"><span style="color:#A6ACCD;">// \u5728main.ts\u4E2D\u5BFC\u5165 \u4F7F\u7528</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">import CaokunTsvue3UI from &#39;caokun-tsvue3-ui&#39;;</span></span>
+<span class="line"><span style="color:#A6ACCD;">import &#39;../node_modules/caokun-tsvue3-ui/lib/style.css&#39;;</span></span>
+<span class="line"><span style="color:#A6ACCD;">const app = createApp(App);</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">app.use(CaokunTsvue3UI).mount(&#39;#app&#39;);</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div><h2 id="\u5BFC\u5165\u540E\u518D\u9875\u9762\u4E2D\u4F7F\u7528" tabindex="-1">\u5BFC\u5165\u540E\u518D\u9875\u9762\u4E2D\u4F7F\u7528 <a class="header-anchor" href="#\u5BFC\u5165\u540E\u518D\u9875\u9762\u4E2D\u4F7F\u7528" aria-hidden="true">#</a></h2><div class="language-"><span class="copy"></span><pre><code><span class="line"><span style="color:#A6ACCD;">&lt;template&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">    &lt;hn-info-collapse title=&quot;\u7EC4\u4EF6hn-info-collapse&quot; style=&quot;width: 100%&quot;&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;template #content&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">        &lt;div&gt;hn-info-collapse \u7684\u5185\u5BB9\u533A\u57DF&lt;/div&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;/template&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">    &lt;/hn-info-collapse&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">    &lt;hn-left-jewel-box title=&quot;\u7EC4\u4EF6hn-left-jewel-box&quot; style=&quot;width: 100%&quot;&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;template #content&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">        &lt;div&gt;hn-left-jewel-box \u7684\u5185\u5BB9\u533A\u57DF&lt;/div&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      &lt;/template&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">  &lt;/hn-left-jewel-box&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">&lt;/template&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div>`,6)]))}var u=a(t,[["render",p]]);export{d as __pageData,u as default};
